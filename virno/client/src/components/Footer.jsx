7971b58080243295e206e575d26d47f4ca51919a
sinatra <sinatrakusuma302@gmail.com>
2023-06-22 06:21:59 +0700

    lsp project

---
+++ virno/client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+    <span>
+      virno website made <b>React.js</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ virno/client/src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import Logo from "../img/logo.png";
+
+const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+        virno
+        </div>
+        <div className="links">
+          <Link className="link" to="/">
+            <h6>Beranda</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
